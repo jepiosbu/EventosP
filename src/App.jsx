91e07188b88177291e4449c3a5355ce3e8e15c60
dvillcar <diego.villa@devzeros.com>
2023-09-20 09:49:26 -0500

    ajuste de ref componente caballero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
-import * as componente from './Components/caballero.jsx';
 import { useRef} from "react";
+import {Caballero} from "./Components/caballero";
 
 function App() {
-    const cubo1=useRef(0);
+    let con = 0;
+    const cubo1 = useRef(null);
+    const cubo2 = useRef(null);
+    const cubo3 = useRef(null);
+    const cubo4 = useRef(null);
+
     const img1= (e)=>{
-        if(con==0)
+        if(con===0)
         {
             cubo1.current.style.top="0vw";
             con++
@@ -17,73 +22,78 @@ function App() {
         }
         
       };
-      const img2= (e)=>{
-        if(con==0)
-        {
-            cubo2.current.style.top="0vw";
-            con++
-        }
-        else
-        {
-            cubo2.current.style.top="23.5vw";
-            con--
-        }
-        
-      };
-      const img3= (e)=>{
-        if(con==0)
-        {
-            cubo3.current.style.top="0vw";
-            con++
-        }
-        else
-        {
-            cubo3.current.style.top="23.5vw";
-            con--
-        }
-        
-      };
-      const img4= (e)=>{
-        if(con==0)
-        {
-            cubo4.current.style.top="0vw";
-            con++
-        }
-        else
-        {
-            cubo4.current.style.top="23.5vw";
-            con--
-        }
-        
-      };
+    const img2= (e)=>{
+    if(con==0)
+    {
+        cubo2.current.style.top="0vw";
+        con++
+    }
+    else
+    {
+        cubo2.current.style.top="23.5vw";
+        con--
+    }
+
+    };
+    const img3= (e)=>{
+    if(con==0)
+    {
+        cubo3.current.style.top="0vw";
+        con++
+    }
+    else
+    {
+        cubo3.current.style.top="23.5vw";
+        con--
+    }
+
+    };
+    const img4= (e)=>{
+    if(con==0)
+    {
+        cubo4.current.style.top="0vw";
+        con++
+    }
+    else
+    {
+        cubo4.current.style.top="1.5vw";
+        con--
+    }
+
+    };
+
   return (
     <main className="App">
-      <componente.Caballero
-    evento={img1}
-    imagen="pegaso.jpg"
-    imagen2="seiya.jpg"
-    contain="containerCaballero1"
-    />
-    <componente.Caballero
-    evento={img2}
-    imagen="andromeda.jpg"
-    imagen2="shun.jpg"
-    contain="containerCaballero2"
-    />
-    <componente.Caballero
-    evento={img3}
-    imagen="shiriu.jpg"
-    imagen2="shiryu.jpg"
-    contain="containerCaballero3"
-    />
-    <componente.Caballero
-    evento={img4}
-    imagen="yoga.jpg"
-    imagen2="hyoga.jpg"
-    contain="containerCaballero4"
-    />
+        <Caballero
+        evento={img1}
+        imagen="pegaso.jpg"
+        imagen2="seiya.jpg"
+        contain="containerCaballero1"
+        cuboRef={cubo1}
+        />
+        <Caballero
+            evento={img2}
+            imagen="andromeda.jpg"
+            imagen2="shun.jpg"
+            contain="containerCaballero2"
+            cuboRef={cubo2}
+        />
+        <Caballero
+            evento={img3}
+            imagen="shiriu.jpg"
+            imagen2="shiryu.jpg"
+            contain="containerCaballero3"
+            cuboRef={cubo3}
+        />
+        <Caballero
+            evento={img4}
+            imagen="yoga.jpg"
+            imagen2="hyoga.jpg"
+            contain="containerCaballero4"
+            cuboRef={cubo4}
+        />
     </main>
   );
 }
 
-export{App,cubo1};
+export{App};
--- src/Components/caballero.jsx
+++ src/Components/caballero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 function Caballero(props) {
+    if (props.cuboRef.current && props.cuboRef.current.style.top === "") {
+        props.cuboRef.current.style.top = "0vw";
+    }
     return  (<section className={props.contain}>
         <div className="cubo" onClick={props.evento}>
             <img className="cara1" src={require(`../Img/${props.imagen}`)} alt="" />
@@ -7,7 +10,7 @@ function Caballero(props) {
             <img className="cara3" src={require(`../Img/${props.imagen}`)} alt="" />
             <img className="cara4" src={require(`../Img/${props.imagen}`)} alt="" />
         </div>
-        <img ref={cubo1} className="caballero" src={require(`../Img/${props.imagen2}`)} alt="" />
+        <img ref={props.cuboRef} className="caballero" src={require(`../Img/${props.imagen2}`)} alt="" />
     </section>);
 }
 export{Caballero}
